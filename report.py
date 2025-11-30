1f7d9a039bdc098491f383f3efc660b7c7c3a76f
varvara.korneeva <varvarakorneeva5202@gmail.com>
2025-12-01 01:28:19 +0300

    Реализация отчета performance

---
+++ report.py
@@ -0,0 +1,72 @@
+import argparse
+import csv
+import os
+import sys
+
+from tabulate import tabulate
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--files', nargs='+')
+    parser.add_argument('--report', type=str)
+
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        print('Введены некорректные аргументы.')
+        sys.exit(1)
+
+    if not args.files or not args.report:
+        print('Не указано значение для аргумента --files или --report.')
+        sys.exit(0)
+
+    for file in args.files:
+        isfile = os.path.isfile(file)
+        if not isfile:
+            print('Файлы не были найдены.')
+            sys.exit(0)
+
+    return args.files, args.report
+
+
+def performance_report(files):
+    position_count = {}
+    for file in files:
+        with open(file, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            next(csv_reader) # Пропускаем первую строку с загаловками
+            for row in csv_reader:
+                if row[1] not in position_count.keys():
+                    position_count[row[1]] = [float(row[3]), 1]
+                else:
+                    position_count[row[1]][0] += float(row[3])
+                    position_count[row[1]][1] += 1
+
+    performance_avg = []
+    for key, value in position_count.items():
+        performance_avg.append((key, round(value[0]/value[1], 2)))
+
+    return performance_avg
+
+
+def get_row_number_for_report(data):
+    for i, row in enumerate(data):
+        data[i] = (i+1,) + row
+
+    return data
+
+
+def get_report(files, report_name):
+    report_data, headers = [], []
+    if report_name == 'performance':
+        report_data = performance_report(files)
+        headers = ['', 'position', 'performance']
+
+    report_data_with_num = get_row_number_for_report(report_data)
+    print(tabulate(report_data_with_num, headers=headers))
+
+
+if __name__ == '__main__':
+    files, report = get_args()
+    get_report(files, report)
---
+++ test_report.py
@@ -0,0 +1,119 @@
+import sys
+
+import pytest
+
+from report import performance_report, get_args, get_report, get_row_number_for_report
+
+
+def test_performance_report_one_file():
+    report = performance_report(['data/employees1.csv'])
+    assert report == [('Mobile Developer', 4.6),
+                      ('Backend Developer', 4.85),
+                      ('DevOps Engineer', 4.7),
+                      ('Frontend Developer', 4.6),
+                      ('Data Engineer', 4.7),
+                      ('QA Engineer', 4.5),
+                      ('Data Scientist', 4.7)]
+
+
+def test_performance_report_few_files():
+    report = performance_report(['data/employees1.csv', 'data/employees2.csv'])
+    assert report == [('Mobile Developer', 4.6),
+                      ('Backend Developer', 4.83),
+                      ('DevOps Engineer', 4.8),
+                      ('Frontend Developer', 4.65),
+                      ('Data Engineer', 4.7),
+                      ('QA Engineer', 4.5),
+                      ('Data Scientist', 4.65),
+                      ('Fullstack Developer', 4.7)]
+
+
+@pytest.fixture
+def mock_argv(request):
+    curr_argv = sys.argv
+    sys.argv = request.param
+    yield
+    sys.argv = curr_argv
+
+
+@pytest.mark.parametrize('mock_argv',
+                         [['report.py', '--files', 'data/employees1.csv', 'data/employees2.csv',
+                           '--report', 'performance']],
+                         indirect=True)
+def test_read_args_right_args(mock_argv):
+    files, report_name = get_args()
+    assert files == ['data/employees1.csv', 'data/employees2.csv'] and report_name == 'performance'
+
+
+@pytest.mark.parametrize('mock_argv',
+                         [['report.py', '--files', 'data/not_exists_file.csv', '--report', 'performance']],
+                         indirect=True)
+def test_read_args_wrong_file(mock_argv, capsys):
+    with pytest.raises(SystemExit) as exception:
+        _, _ = get_args()
+    captured = capsys.readouterr()
+    assert captured.out == 'Файлы не были найдены.\n'
+    assert exception.value.code == 0
+
+
+@pytest.mark.parametrize('mock_argv',
+                         [['report.py']],
+                         indirect=True)
+def test_read_args_without_args(mock_argv, capsys):
+    with pytest.raises(SystemExit) as exception:
+        _, _ = get_args()
+    captured = capsys.readouterr()
+    assert captured.out == 'Не указано значение для аргумента --files или --report.\n'
+    assert exception.value.code == 0
+
+
+@pytest.mark.parametrize('mock_argv',
+                         [['report.py', '--files', 'daata/employees1.csv']],
+                         indirect=True)
+def test_read_args_only_files(mock_argv, capsys):
+    with pytest.raises(SystemExit) as exception:
+        _, _ = get_args()
+    captured = capsys.readouterr()
+    assert captured.out == 'Не указано значение для аргумента --files или --report.\n'
+    assert exception.value.code == 0
+
+
+@pytest.mark.parametrize('mock_argv',
+                         [['report.py', '--report', 'performance']],
+                         indirect=True)
+def test_read_args_only_report(mock_argv, capsys):
+    with pytest.raises(SystemExit) as exception:
+        _, _ = get_args()
+    captured = capsys.readouterr()
+    assert captured.out == 'Не указано значение для аргумента --files или --report.\n'
+    assert exception.value.code == 0
+
+
+@pytest.mark.parametrize('mock_argv',
+                         [['report.py', '--unknown', 'arg']],
+                         indirect=True)
+def test_read_args_unknown_arg(mock_argv, capsys):
+    with pytest.raises(SystemExit) as exception:
+        _, _ = get_args()
+    captured = capsys.readouterr()
+    assert captured.out == 'Введены некорректные аргументы.\n'
+    assert exception.value.code == 1
+
+
+def test_output_report(capsys):
+    get_report(['data/employees1.csv'], 'performance')
+    captured = capsys.readouterr()
+    assert captured.out == '    position              performance\n' \
+                           '--  ------------------  -------------\n' \
+                           ' 1  Mobile Developer             4.6\n' \
+                           ' 2  Backend Developer            4.85\n' \
+                           ' 3  DevOps Engineer              4.7\n' \
+                           ' 4  Frontend Developer           4.6\n' \
+                           ' 5  Data Engineer                4.7\n' \
+                           ' 6  QA Engineer                  4.5\n' \
+                           ' 7  Data Scientist               4.7\n'
+
+
+def test_get_row_number_for_report():
+    data_with_num = get_row_number_for_report([('Mobile Developer', 4.6), ('Backend Developer', 4.85)])
+    assert data_with_num == [(1, 'Mobile Developer', 4.6), (2, 'Backend Developer', 4.85)]
