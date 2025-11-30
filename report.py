8dfbe48e8fc61de0ffef304bd2588d2c23f3f171
varvara.korneeva <varvarakorneeva5202@gmail.com>
2025-12-01 01:51:19 +0300

    Комментарии

--- report.py
+++ report.py
@@ -35,13 +35,13 @@ def performance_report(files):
     for file in files:
         with open(file, 'r') as csvfile:
             csv_reader = csv.reader(csvfile)
-            next(csv_reader) # Пропускаем первую строку с загаловками
+            next(csv_reader) # Пропускаем первую строку с заголовками
             for row in csv_reader:
                 if row[1] not in position_count.keys():
                     position_count[row[1]] = [float(row[3]), 1]
                 else:
-                    position_count[row[1]][0] += float(row[3])
-                    position_count[row[1]][1] += 1
+                    position_count[row[1]][0] += float(row[3]) # Сумма performance
+                    position_count[row[1]][1] += 1 # Количество записей
 
     performance_avg = []
     for key, value in position_count.items():
@@ -59,6 +59,7 @@ def get_row_number_for_report(data):
 
 def get_report(files, report_name):
     report_data, headers = [], []
+    # Отчет performance
     if report_name == 'performance':
         report_data = performance_report(files)
         headers = ['', 'position', 'performance']
